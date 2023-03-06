7d351f6342a9b3b5429601e433408fab3b55d5cf
Ravi Kumar <gravi625@gmail.com>
2023-03-06 14:08:42 +0530

    Reading Values from Dictionary

--- sortArithComputation.sh
+++ sortArithComputation.sh
@@ -10,6 +10,8 @@ ans2=$((c+a/b))
 echo answer2: $ans2
 ans3=$((a%b+c))
 echo answer3: $ans3
+declare -A test_dict
 test_dict[value1]=$ans
 test_dict[value2]=$ans2
 test_dict[value3]=$ans3
+echo ${test_dict[@]}
